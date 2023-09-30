42fa071a0d9e2ceb093477c88ab53f56a09180fe
Frank Weng <lpvvnnoobs@gmail.com>
2023-09-30 16:43:44 -0500

    Adding zoom functionality and changed the arena to a floating bowl

--- scripts/CameraJoint.gd
+++ scripts/CameraJoint.gd
@@ -1,11 +1,23 @@
 class_name CameraJoint extends SpringArm3D
 
 @export var mouse_sensitivity: float = 0.05
-@export var joystick_sensitivity: float = 45.0
+@export var joystick_sensitivity: float = 90.0
 
 func _ready():
 	set_as_top_level(true)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+@export_category("Zoom")
+@export var zoom_speed: float = 2.0 ** (1.0 / 20.0)
+@export var joy_zoom_speed: float = 8.0
+@export var zoom_ease_duration = 0.25
+@export var zoom_clamp_min: float = 2.0
+@export var zoom_clamp_max: float = 10.0
+@onready var zoom_setpoint: float = spring_length:
+	get:
+		return zoom_setpoint
+	set(value):
+		zoom_setpoint = clamp(value, zoom_clamp_min, zoom_clamp_max)
+		create_tween().tween_property(self, "spring_length", zoom_setpoint, zoom_ease_duration)
 
 func rotate_camera(input: Vector2):
 	rotation_degrees.x += -input.y
@@ -18,10 +30,19 @@ func _unhandled_input(event: InputEvent):
 	if event is InputEventMouseMotion:
 		var mouse_camera = event.relative * mouse_sensitivity
 		rotate_camera(mouse_camera)
+	if event is InputEventMouseButton:
+		match event.button_index:
+			MOUSE_BUTTON_WHEEL_UP:
+				zoom_setpoint /= zoom_speed
+			MOUSE_BUTTON_WHEEL_DOWN:
+				zoom_setpoint *= zoom_speed
 
 func _process(delta):
 	position = get_parent().position
 	var joy_camera = Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down") * joystick_sensitivity * delta
 	rotate_camera(joy_camera)
+	var zoom: float = Input.get_axis("zoom_in", "zoom_out")
+	zoom_setpoint *= (joy_zoom_speed ** (zoom * delta))
+	
 	
 	
