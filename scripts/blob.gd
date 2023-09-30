5d0c6fc01f7628d5c9a046dbf8a497f2995fa7d9
Frank Weng <lpvvnnoobs@gmail.com>
2023-09-30 00:10:53 -0500

    Adding a little demo for blob cohesion

--- scripts/blob.gd
+++ scripts/blob.gd
@@ -1,28 +1,39 @@
 class_name Blob extends Node2D
 
 const particle_class : PackedScene = preload("res://prefabs/particle_ball.tscn")
+const E = 2.71828182845904523536028747135266249775724709369995
 
 @export var num_particles: int = 50
-@export var cohesive_force: float = 1000
+@export var cohesive_force: float = 100
 @export var use_camera: bool = true
 
 var particles : Array[RigidBody2D]
 var camera: Camera2D
 func _ready():
+	# instantiate particles in a spiral so they don't immediately collide as much
 	particles = []
-	for x in num_particles:
+	const loops = 2
+	const radius = 100
+	var final = E ** loops
+	for i in range(num_particles, 0, -1):
 		var particle = particle_class.instantiate()
+		var magnitue = log(final * i / num_particles)
+		var rot = magnitue * TAU 
+		var orientation = Vector2(cos(rot), -sin(rot))
+		var length = radius * magnitue / loops
+		particle.position = orientation * length
 		add_child(particle)
 		particles.append(particle)
 	if use_camera:
 		camera = Camera2D.new()
+		camera.position_smoothing_enabled = true
 		add_child(camera)
 	else:
 		camera = null
 		
 	var tween = create_tween()
-	tween.tween_property(self, "cohesive_force", 10, 1)
-	tween.tween_property(self, "cohesive_force", 10000, 1)
+	tween.tween_property(self, "cohesive_force", 10000, 5)
+	tween.tween_property(self, "cohesive_force", -1000, 5)
 	tween.set_loops()
 
 
