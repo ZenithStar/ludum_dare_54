3b0252d39d7444d6d13923e18dceb5d330944f38
Frank Weng <lpvvnnoobs@gmail.com>
2023-10-01 12:02:21 -0500

    Changing machines

--- scripts/RollingBall.gd
+++ scripts/RollingBall.gd
@@ -1,8 +1,23 @@
 class_name RollingBall extends RigidBody3D
 
-@export var force_factor: float = 10.0
+@export var force_factor: float = 20.0
 
-func _physics_process(delta):
+var force_vector: Vector3 = Vector3.ZERO
+
+func _process(delta):
+	var force_vector_display = force_vector / force_factor * 2.0
+	$ForceVector.position = position + (force_vector_display / 2.0)
+	$ForceVector.height = force_vector_display.length()
+	$ForceVector.look_at(force_vector_display)
+
+func _integrate_forces(state):
+	var mean_normal = Vector3.ZERO
+	for x in state.get_contact_count():
+		mean_normal += state.get_contact_impulse(x)
+	mean_normal = mean_normal.normalized()
+	var force_plane = Plane(mean_normal)
 	var force_input = Input.get_vector("move_left", "move_right", "move_forward", "move_back").rotated(-$CameraJoint.rotation.y)
+	var force_direction = force_plane.project(Vector3(force_input.x, 0.0, force_input.y)).normalized()
+	force_vector = force_direction * clamp(force_input.length(), 0.0, 1.0) * force_factor
+	apply_central_force(force_vector * state.get_step())
 	
-	apply_central_force(Vector3(force_input.x, 0.0, force_input.y) * force_factor * delta)
---
+++ scripts/Spawner.gd
@@ -0,0 +1,11 @@
+class_name Spawner extends Node3D
+
+const particle_class : PackedScene = preload("res://prefabs/particle_ball.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
