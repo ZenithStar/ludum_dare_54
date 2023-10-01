8a1d6f127b888634599ff9909bb15fabd50dae6c
Frank Weng <lpvvnnoobs@gmail.com>
2023-10-01 17:27:38 -0500

    moving machines again

--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -1,11 +1,23 @@
 class_name Spawner extends Node3D
 
-const particle_class : PackedScene = preload("res://prefabs/particle_ball.tscn")
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+const ball_class : PackedScene = preload("res://prefabs/play_ball.tscn")
+@export var radius_upper_limit: float = 10.0
+@export var radius_lower_limit: float = 5.0
+@export var exit_force_range: float = 1000.0
+@export var noise: FastNoiseLite
+@onready var elapsed = 0.0
+@onready var accumulation = 0.0
+@onready var orientation = Vector2(1.0,0.0)
+@export var spawn_rate: float = 1.0
+@export var spin_rate: float = 1.0
+@export var displacement_rate: float = 1.0
+func _physics_process(delta):
+	accumulation += noise.get_noise_1d(elapsed) * delta
+	var angle = atan2(orientation.y, orientation.x)
+	angle += noise.get_noise_1d(elapsed)
+	print(noise.get_noise_1d(elapsed))
+	while accumulation > spawn_rate:
+		accumulation -= spawn_rate
+		var ball = ball_class.instantiate()
+		ball.apply_central_force(Vector3())
+	elapsed += delta
